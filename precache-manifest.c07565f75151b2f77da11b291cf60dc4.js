da66c81096f517084a2ea3867c200966de89dff0
anshul kothari <anshul@anshuls-MacBook-Pro.local>
2020-01-26 16:50:05 +0530

    Updates

---
+++ precache-manifest.c07565f75151b2f77da11b291cf60dc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5afad9f90b608b841db80712626d3e",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "de24ce44f79efef56c85",
+    "url": "/shoppingCart/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "8d6e395ac8997dd9cb5a",
+    "url": "/shoppingCart/static/css/main.9cf7d10a.chunk.css"
+  },
+  {
+    "revision": "de24ce44f79efef56c85",
+    "url": "/shoppingCart/static/js/2.211ecbcf.chunk.js"
+  },
+  {
+    "revision": "79d8274d4bbf05b376a2b51a213ecb0a",
+    "url": "/shoppingCart/static/js/2.211ecbcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d6e395ac8997dd9cb5a",
+    "url": "/shoppingCart/static/js/main.245748da.chunk.js"
+  },
+  {
+    "revision": "801e007f393a2231dfcb",
+    "url": "/shoppingCart/static/js/runtime-main.38bfcc13.js"
+  },
+  {
+    "revision": "7d65a2eacc249663292e2336e88940e2",
+    "url": "/shoppingCart/static/media/instagram-icon.7d65a2ea.png"
+  }
+]);
