59091f409e5ff598b5b821f1debae0df58142efc
anshul kothari <anshul@anshuls-MacBook-Pro.local>
2020-01-26 22:18:00 +0530

    improved cart functionality

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
 	<Provider store={store()}>
 	  <Router basename={process.env.PUBLIC_URL}>
 	  	<Route exact path="/" component={App} />
-	    <Route path="/cart" component={Cart} />
+	    <Route exact path="/cart" component={Cart} />
 	  </Router>
 	</Provider>,
   document.getElementById('root')
